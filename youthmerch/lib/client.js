2d711acd3b266e61a2af709b359bbcc916631de4
Naveen Bhatti <jrbhrn@gmail.com>
2023-03-21 19:30:28 -0500

    Add initial sanity files

---
+++ youthmerch/lib/client.js
@@ -0,0 +1,14 @@
+import sanityClient from '@sanity/client';
+import  imageUrlBuilder  from '@sanity/image-url';
+
+export const client = sanityClient({
+  projectId: '94z1jt5w',
+  dataset: 'production',
+  apiVersion: '2023-03-21',
+  useCdn: true,
+  token:process.env.NEXT_PUBLIC_TOKEN
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
