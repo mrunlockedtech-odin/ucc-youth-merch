680954a151faf0124db765c3aa306a98c65cc8fb
Naveen Bhatti <jrbhrn@gmail.com>
2023-03-22 12:48:47 -0500

    Add image selector and additional information to Product Details Page

--- youthmerch/src/pages/product/[slug].js
+++ youthmerch/src/pages/product/[slug].js
@@ -1,26 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 
-import { client, urlFor } from '../../../lib/client'
+import { client, urlFor } from '../../../lib/client';
+import { Product } from '../../../components';
 
 const ProductDetails = ( {product, products}) => {
   const {image, name, details, price} = product;
+  const [index, setIndex] = useState(0)
   return (
     <div>
       <div className="product-detail-container">
         <div>
           <div className="image-container">
-            <img src={urlFor(image && image[0])} width={600}/>
+            <img src={urlFor(image && image[index])} className="product-detail-image"/>
           </div>
-          {/*<div className="small-images-container">
+          <div className="small-images-container">
             {image?.map((item,i) => (
               <img
                 src={urlFor(item)}
-                className=""
-                onMouseEnter=""
+                className={i === index ? 'small-image selected-image' : 'small-image'}
+                onMouseEnter={() => setIndex(i)}
               />
             ))}
-            </div>*/}
+            </div>
         </div>
         <div className="product-detail-desc">
           <h1>{name}</h1>
@@ -52,6 +54,16 @@ const ProductDetails = ( {product, products}) => {
             <button type="button" className="buy-now" onClick="">Buy Now</button>
           </div>
         </div>
+      </div>
+      <div className="maylike-products-wrapper">
+            <h2>You may also like</h2>
+            <div className="marquee">
+              <div className="maylike-products-container track">
+                {products.map((item) => (
+                  item.slug.current != product.slug.current ? <Product key={item._id} product={item}/> : <></>))
+                }
+              </div>
+            </div>
       </div>
     </div>
   )
