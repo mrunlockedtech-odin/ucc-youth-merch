c077f88989d309e7548996711648a887bf481694
Naveen Bhatti <jrbhrn@gmail.com>
2023-03-22 11:08:57 -0500

    Add basic footer

--- youthmerch/components/Footer.jsx
+++ youthmerch/components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { AiFillInstagram } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p>2023 UCC Youth  All Rights Reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+      </p>
+    </div>
   )
 }
 
