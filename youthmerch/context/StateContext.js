24c900296cdfb14e0559321065dbda9b53202443
Naveen Bhatti <jrbhrn@gmail.com>
2023-03-23 13:38:23 -0500

    Add quantity changing to cart

--- youthmerch/context/StateContext.js
+++ youthmerch/context/StateContext.js
@@ -1,26 +1,30 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
 
 import { toast } from 'react-hot-toast';
+import product from '../youthmerch/schemas/product';
 
 const Context = createContext();
 
 
-export const StateContext = ( { children }) => {
+export const StateContext = ({ children }) => {
   const [showCart, setShowCart] = useState(false)
   const [cartItems, setCartItems] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
   const [totalQuantities, setTotalQuantities] = useState(0)
   const [qty, setQty] = useState(1);
 
+  let foundProduct;
+  let index;
+
   const onAdd = (product, quantity) => {
     const checkProductInCart = cartItems.find((item) => item._id === product._id);
-    
+
     setTotalPrice((prevTotalPrice) => prevTotalPrice + product.price * quantity);
     setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
-    if(checkProductInCart){
+    if (checkProductInCart) {
 
       const updatedCartItems = cartItems.map((cartProduct) => {
-        if(cartProduct._id === product._id) return {
+        if (cartProduct._id === product._id) return {
           ...cartProduct,
           quantity: cartProduct.quantity + quantity
         }
@@ -29,23 +33,41 @@ export const StateContext = ( { children }) => {
       setCartItems(updatedCartItems)
     } else {
       product.quantity = quantity;
-      setCartItems([...cartItems, {...product}])
+      setCartItems([...cartItems, { ...product }])
     }
     toast.success(`${qty} ${product.name} added to the cart.`);
   }
 
+  const toggleCartItemQuantity = (id, value) => {
+    foundProduct = cartItems.find((item) => item._id === id)
+    index = cartItems.findIndex((product) => product._id === id)
+    const newCartItems = cartItems.filter((item) => item._id !== id)
+
+    if (value === "inc") {
+      setCartItems([...cartItems.slice(0, index), { ...foundProduct, quantity: foundProduct.quantity + 1 }, ...cartItems.slice(index + 1)]);
+      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price);
+      setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + 1);
+    } else if (value === "dec") {
+      if (foundProduct.quantity > 1) {
+        setCartItems([...cartItems.slice(0, index), { ...foundProduct, quantity: foundProduct.quantity - 1 }, ...cartItems.slice(index + 1)]);
+        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price);
+        setTotalQuantities((prevTotalQuantities) => prevTotalQuantities - 1);
+      }
+    }
+  }
+
   const incQty = () => {
-    setQty((prevQty) =>  prevQty + 1 )
+    setQty((prevQty) => prevQty + 1)
   }
   const decQty = () => {
     setQty((prevQty) => {
-      if(prevQty-1 < 1) return 1;
-      return prevQty -1;
-    } )
+      if (prevQty - 1 < 1) return 1;
+      return prevQty - 1;
+    })
   }
 
-  return(
-    <Context.Provider 
+  return (
+    <Context.Provider
       value={{
         showCart,
         cartItems,
@@ -56,6 +78,7 @@ export const StateContext = ( { children }) => {
         decQty,
         onAdd,
         setShowCart
+        , toggleCartItemQuantity
       }}>
       {children}
     </Context.Provider>
