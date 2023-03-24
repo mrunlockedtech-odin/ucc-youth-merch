5b2c6c4377b101bdffa058170864354da5622ae1
Naveen Bhatti <jrbhrn@gmail.com>
2023-03-24 14:46:27 -0500

    Add stripe testing integration to project

--- youthmerch/lib/client.js
+++ youthmerch/lib/client.js
@@ -6,7 +6,7 @@ export const client = sanityClient({
   dataset: 'production',
   apiVersion: '2023-03-21',
   useCdn: true,
-  token:process.env.NEXT_PUBLIC_TOKEN
+  token:process.env.TOKEN
 });
 
 const builder = imageUrlBuilder(client);
