cae304ba8ddae172dd6b2dcda52d75be7159dfc4
Naveen Bhatti <jrbhrn@gmail.com>
2023-03-27 14:33:27 -0500

    Implement filter function on All Products page

--- youthmerch/pages/allproducts.js
+++ youthmerch/pages/allproducts.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { client } from '../lib/client';
 import { Product } from '../components';
 
 const allProducts = ({ products}) => {
+  const[selectedProducts,setSelectedProducts] = useState(products)
+  const productTypes = Array.from (new Set(products.map((product) => product.type)))
+
+  const handleTypeSelection = (e) => {
+    setSelectedProducts(products.filter(product => product.type === e.target.value || e.target.value === 'All Items'))
+  }
   return (
     <>
       <div className="products-heading">
         <h2>All Products</h2>
       </div>
+
+      <div className="all-products-selection">
+        <label for="product-type"> Filter By</label>
+        <select name="product-type" id="product-type" className="product-selection" onChange={handleTypeSelection}>
+          <option value="All Items">All Items</option>
+          {productTypes?.map((productType) => <option value={productType}>{productType}</option>)}
+          {console.log("ProductTypes", productTypes)}
+        </select>
+      </div>
+
       <div className="products-container gridProducts">
-        {products?.map((product) => <Product key={product._id} product={product}/>)}
-        {console.log(products)}
+        {selectedProducts?.map((product) => <Product key={product._id} product={product}/>)}
       </div>
     </>
   )
