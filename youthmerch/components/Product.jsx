8920814f4c2c2fa9e6ef2f1f4a5971fb9f6952f2
Naveen Bhatti <jrbhrn@gmail.com>
2023-03-31 15:56:18 -0500

    Create random function to change pictures on index page

--- youthmerch/components/Product.jsx
+++ youthmerch/components/Product.jsx
@@ -8,7 +8,7 @@ const Product = ({ product: { image, name, slug, price }}) => {
     <div>
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
-          <img src={urlFor(image && image[0])} alt={name} width={250} height={250} className="product-image" />
+          <img src={urlFor(image && image[Math.floor(Math.random() * (image.length-3 - 0 + 1)) + 0])} alt={name} width={250} height={250} className="product-image" />
           <p className="product-name">{name}</p>
           <p className="product-price">${price}</p>
         </div>
