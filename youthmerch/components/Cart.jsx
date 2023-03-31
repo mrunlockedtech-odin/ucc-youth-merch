945b84f9ec7865971206e5c1a3bd0464e2f441e8
Naveen Bhatti <jrbhrn@gmail.com>
2023-03-31 15:29:52 -0500

    Make cart images match user selections

--- youthmerch/components/Cart.jsx
+++ youthmerch/components/Cart.jsx
@@ -60,12 +60,11 @@ const Cart = () => {
         <div className="product-container">
           {cartItems.length >= 1 && cartItems.map((item, index) => (
             <div className="product" key={item._id}>
-              <img src={urlFor(item?.image[0])} className="cart-product-image" />
+              <img src={urlFor(item?.image.filter(img => img.caption === item.description.substring(2, item.description.indexOf(",")))[0])} className="cart-product-image" />
               <div className="item-desc">
                 <div className="flex top">
                   <h5>{item.name}</h5>
                   <h4>${item.price}</h4>
-
                 </div>
                 <h6>{item.description}</h6>
                 <div className="flex bottom">
